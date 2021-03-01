e4e15d662b7a6ebf930dbc7a6a0f1fe5dc69a099
canina1337 <46906916+canina1337@users.noreply.github.com>
2021-03-01 14:19:51 +0300

    Update dna.js

--- dna.js
+++ dna.js
@@ -1,23 +1,32 @@
 function Complite()
-        {
-            let list = [];
-            let elem = document.formDNA.DNA.value;
-            let array = elem.split(/(?=[A-Z])/);
-            
-            for (let index = 0; index < array.length; index++) {
-                element = array[index];
-                console.log(element);
-                if (element == 'G') {
-                    element = 'C';
-                }else if (element == 'C') {
-                    element = 'G';
-                }else if (element == 'T') {
-                    element = 'A';
-                }else if (element == 'A') {
-                    element = 'U';
-                }
-                list.push(element);
-            }
-            alert(list);
-        
-        };
+    {
+      let list = [];
+      let elem = document.formDNA.DNA.value;
+      let array = elem.split(/(?=[A-Z])/);
+      
+      if (array == false) {
+        alert('Введите значения');
+      } else {
+        for (let index = 0; index < array.length; index++) {
+          element = array[index];
+          console.log(element);
+          
+          if (element == 'G') {
+            element = 'C';
+          }else if (element == 'C') {
+            element = 'G';
+          }else if (element == 'T') {
+            element = 'A';
+          }else if (element == 'A') {
+            element = 'U';
+          }else {
+            return alert('Введите корректное значение');
+          } 
+          list.push(element);
+        }
+        if (list) {
+          alert(list);
+          console.log(list);
+        }      
+      }  
+    };
